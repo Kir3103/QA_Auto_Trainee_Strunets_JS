f12099c6bdca83d1775c0fbbafe26547fc03e22e
Kir3103 <strunets96@gmail.com>
2023-05-12 01:03:07 +0300

    task2..task4

---
+++ task1/task1.js
@@ -0,0 +1,15 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question('Введите необходимое число: ', number=> {
+    if(isNaN(number.trim()) || number.trim().length < 1){
+        console.log('Повторите попытку ввода числа.');
+    } else if(Number(number.trim()) > 7){
+        console.log('Привет.');
+    };
+
+readline.close();
+
+});
